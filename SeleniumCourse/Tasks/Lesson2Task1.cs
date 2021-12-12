cb1aad175f239efe57f4c72d8feb1d01a51ca7c2
Petr Danilov <Petr.Danilov@emerson.com>
2021-12-12 20:39:36 +0300

    Get started, Lesson 2- task 1

---
+++ SeleniumCourse/Tasks/Lesson2Task1.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace SeleniumCourse
+{
+    public class Tests
+    {
+        private WebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void Test()
+        {
+            this.driver.Url = "https://www.google.com/";
+            //Agree to use cookies
+            this.driver.FindElement(By.XPath("//div[. = 'I agree']")).Click();
+            this.driver.FindElement(By.Name("q")).SendKeys("webdriver\n");
+            this.wait.Until(c => c.FindElement(By.XPath("//*[contains(., 'WebDriver')]")));
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
