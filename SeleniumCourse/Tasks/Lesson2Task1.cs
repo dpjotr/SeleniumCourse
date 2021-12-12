7490292dd6ae1da7c41738a947f1a0785e7791e3
Danilov Pjotr <danilovpjotr@gmail.com>
2021-12-13 00:16:19 +0300

    Fixed braces formatting in Lesson 2- task 1

--- SeleniumCourse/Tasks/Lesson2Task1.cs
+++ SeleniumCourse/Tasks/Lesson2Task1.cs
@@ -8,39 +8,39 @@ namespace SeleniumCourse.Tasks
 {
     public class Lesson2Task1
     {
-        private WebDriver driver;
-    private WebDriverWait wait;
+            private WebDriver driver;
+        private WebDriverWait wait;
 
-    [SetUp]
-    public void Setup()
-    {
-        driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void Test()
+        {
+            this.driver.Url = "https://www.google.com/";
+                //Agree to use cookies
+                try
+                {
+                    //Agree to use cookies if asked
+                    this.driver.FindElement(By.XPath("//div[. = 'I agree']")).Click();
+                }
+                catch (OpenQA.Selenium.NoSuchElementException e)
+                {                
+                }
+
+            this.driver.FindElement(By.Name("q")).SendKeys("webdriver\n");
+            this.wait.Until(c => c.FindElement(By.XPath("//*[contains(., 'WebDriver')]")));
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Quit();
+            driver = null;
+        }
     }
-
-    [Test]
-    public void Test()
-    {
-        this.driver.Url = "https://www.google.com/";
-            //Agree to use cookies
-            try
-            {
-                //Agree to use cookies if asked
-                this.driver.FindElement(By.XPath("//div[. = 'I agree']")).Click();
-            }
-            catch (OpenQA.Selenium.NoSuchElementException e)
-            {                
-            }
-
-        this.driver.FindElement(By.Name("q")).SendKeys("webdriver\n");
-        this.wait.Until(c => c.FindElement(By.XPath("//*[contains(., 'WebDriver')]")));
-    }
-
-    [TearDown]
-    public void CleanUp()
-    {
-        driver.Quit();
-        driver = null;
-    }
-}
 }
