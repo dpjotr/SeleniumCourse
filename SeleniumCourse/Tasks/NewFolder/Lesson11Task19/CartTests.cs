e7d80ea26587cc06e6e2c51a9b02517d22b77014
Petr Danilov <Petr.Danilov@emerson.com>
2022-01-21 18:45:58 +0300

    Lesson11-Task19 get started

---
+++ SeleniumCourse/Tasks/NewFolder/Lesson11Task19/CartTests.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumCourse.Tasks.NewFolder.Lesson11Task19
+{
+    class CartTests : TestBase
+    {
+        [Test]
+        public void Test()
+        {
+            while (this.cartItemCounter < 3)
+            {
+                this.AddFirstPoularProductToCart();
+            }
+
+            this.driver.FindElement(By.CssSelector("#cart a.link")).Click();
+
+            while (this.driver.FindElements(By.CssSelector("#box-checkout-cart ul.shortcuts li.shortcut")).Count > 1)
+            {
+                this.driver.FindElement(By.CssSelector("#box-checkout-cart ul.shortcuts > li > a")).Click();
+
+                var summaryTableRowAmount = this.driver.FindElements(
+                    By.CssSelector("#box-checkout-summary table tr")).Count;
+
+                this.driver.FindElement(
+                        By.CssSelector("#box-checkout-cart button[name=\"remove_cart_item\"]")).Click();
+
+                --summaryTableRowAmount;
+
+                wait.Until((driver) =>
+                    driver.FindElements(By.CssSelector("#box-checkout-summary table tr")).Count == summaryTableRowAmount);
+            }
+
+            var itemToDeleteRemoveButton = this.driver.FindElement(
+                    By.CssSelector("#box-checkout-cart button[name=\"remove_cart_item\"]"));
+
+            itemToDeleteRemoveButton.Click();
+
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.StalenessOf(itemToDeleteRemoveButton));
+        }
+
+        // Add first product from the list of the most popular products to cart
+        private void AddFirstPoularProductToCart()
+        {
+            this.driver.Url = "http://localhost/litecart/en/";
+
+            this.driver.FindElement(By.CssSelector("div#box-most-popular.box ul.products:nth-child(1) a.link")).Click();
+
+            this.cartItemCounter = int.Parse(
+                this.driver.FindElement(By.CssSelector("#cart a.content span.quantity")).GetAttribute("textContent"));
+
+            if (driver.FindElements(By.CssSelector(".options > select[name=\"options[Size]\"]")).Count > 0)
+            {
+                new SelectElement(driver.FindElement(By.CssSelector(".options > select[name=\"options[Size]\"]")))
+                    .SelectByIndex(1);
+            }
+
+            this.driver.FindElement(
+                By.CssSelector("#box-product .information .buy_now .quantity > button[name=\"add_cart_product\"]")).Click();
+
+            wait.Until((driver) =>
+                int.Parse(driver.FindElement(By.CssSelector("#cart a.content span.quantity")).GetAttribute("textContent"))
+                    != cartItemCounter);
+
+            ++this.cartItemCounter;
+        }
+    }
+}
---
+++ SeleniumCourse/Tasks/NewFolder/Lesson11Task19/TestBase.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace SeleniumCourse.Tasks.NewFolder.Lesson11Task19
+{
+    class TestBase
+    {
+        protected WebDriver driver;
+        protected WebDriverWait wait;
+        protected DriverOptions options;
+        protected int cartItemCounter;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }  
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
